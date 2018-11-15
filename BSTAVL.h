cbbc8a0afeb821509bf3fc6539450d6ab8bfba23
sellc <chris.d.sellers@gmail.com>
2018-11-15 10:49:57 -0800

    Rough outline for classes
    Added BarcodeBSTScanner.cpp.
    Modified BSTAVL.h for slightly more complete add and delete functions.
    Added provided code to main.cpp.

--- BSTAVL.h
+++ BSTAVL.h
@@ -14,9 +14,34 @@ template<class T>
 class BSTAVL {
   Node* root;
 public:
-  void addNode(T& data);
-  void deleteNode(T& data);
-  
+  void addNode(T& data){
+    if(root == nullptr){
+      root = new Node(data);
+    } else {
+      addNode(data, root);
+    }
+  }
+  void addNode(T& data, Node* root){
+    if(root == nullptr){
+      root->data = new Node(data);
+    } else if(root->data > data){
+      addNode(data, root->left);
+    } else {
+      addNode(data, root->right);
+    }
+  }
+  void deleteNode(T& data){
+    deleteNode(data, root);
+  }
+  void deleteNode(T& data, Node* root){
+    if(root->data == data){
+      delete root;
+    } else if(root->data > data){
+      deleteNode(data, root->left);
+    } else {
+      deleteNode(data, root->right);
+    }
+  }
 };
 
 #endif
